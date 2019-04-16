251d8f0f3131e456cc0b03e306fed6d012054374
Daniel Haynes <daniel.haynes@cms.hhs.gov>
2019-04-16 12:51:39 -0400

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-include_controls 'Oracle Database 12c Security Technical Implementation Guide' do
+include_controls 'mongodb-enterprise-advanced-3-stig-baseline' do
   control 'V-61409' do
 
   end
